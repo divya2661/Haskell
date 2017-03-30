5cbe5cb69347d3c59b79820c92b746b40ccee1fd
Divya <divya.nagar@juspay.in>
2017-03-30 23:10:03 +0530

    recursive functions

---
+++ Learn-you-haskell/l-functions.hs
@@ -0,0 +1,11 @@
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors a b = (fst a + fst b, snd a + snd b)
+
+first :: (a, b, c) -> a  
+first (x, _, _) = x  
+  
+second :: (a, b, c) -> b  
+second (_, y, _) = y  
+  
+third :: (a, b, c) -> c  
+third (_, _, z) = z  
